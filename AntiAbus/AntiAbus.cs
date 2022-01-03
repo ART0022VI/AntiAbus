4f93aef1fa3884e42ff36ff3daead920a5ae04c3
KoT0XleB <96589180+KoT0XleB@users.noreply.github.com>
2022-01-03 17:00:19 +0500

    Add files via upload

---
+++ AntiAbus/AntiAbus.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Qurre;
+using Qurre.API;
+using Qurre.API.Events;
+using MEC;
+using Qurre.API.Objects;
+using Qurre.API.Controllers;
+using System.Collections.Generic;
+using static AntiAbus.Config;
+
+namespace AntiAbus
+{
+    public class AntiAbus : Plugin
+    {
+        public static bool Enabled { get; internal set; }
+        public override string Developer => "KoToXleB#4663";
+        public override string Name => "AntiAbus";
+        public override Version Version => new Version(1, 0, 0);
+        public override int Priority => int.MinValue;
+        public override void Enable() => RegisterEvents();
+        public override void Disable() => UnregisterEvents();
+        public Config CustomConfig { get; private set; }
+        public void RegisterEvents()
+        {
+            CustomConfig = new Config();
+            CustomConfigs.Add(CustomConfig);
+            if (!CustomConfig.IsEnable) return;
+
+            Qurre.Events.Server.SendingRA += OnSendRA;
+        }
+        public void UnregisterEvents()
+        {
+            CustomConfigs.Remove(CustomConfig);
+            if (!CustomConfig.IsEnable) return;
+
+            Qurre.Events.Server.SendingRA -= OnSendRA;
+        }
+        public void OnSendRA(SendingRAEvent ev)
+        {
+            if (!(ev.CommandSender.SenderId == null))
+            {
+                if (!(ev.CommandSender.SenderId == "1"))
+                {
+                    if (!CustomConfig.admins.ContainsKey(ev.CommandSender.SenderId) && CustomConfig.admins.ContainsKey($"{ev.Player.GroupName}"))
+                    {
+                        CustomConfig.admins.Add(ev.CommandSender.SenderId, new Admin());
+                    }
+                    if (!Round.Started)
+                    {
+                        if (CustomConfig.IsRoundNotStarted)
+                        {
+                            ev.ReplyMessage = CustomConfig.RoundNotStartedMessage;
+                            ev.Success = false;
+                            ev.Allowed = false;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        if (Round.ElapsedTime.Minutes < CustomConfig.NeedTimeMinutes)
+                        {
+                            ev.ReplyMessage = $"<color=red>Time > {CustomConfig.Wait} {CustomConfig.NeedTimeMinutes * 60 - Round.ElapsedTime.Seconds} {CustomConfig.Seconds}</color>";
+                            ev.Success = false;
+                            ev.Allowed = false;
+                            return;
+                        }
+                        if (ev.Name == "give")
+                        {
+                            if (ev.Args[1] == "16")
+                            {
+                                ev.ReplyMessage = CustomConfig.GiveHidMessage;
+                                ev.Success = false;
+                                ev.Allowed = false;
+                                return;
+                            }
+                            if (CustomConfig.admins[ev.CommandSender.SenderId].give >= CustomConfig.admins[$"{ev.Player.GroupName}"].give)
+                            {
+                                ev.ReplyMessage = CustomConfig.LimitGiveMessage;
+                                ev.Success = false;
+                                ev.Allowed = false;
+                            }
+                            else CustomConfig.admins[ev.CommandSender.SenderId].give++;
+                        }
+
+                        if (ev.Name == "forceclass")
+                        {
+                            if (CustomConfig.admins[ev.CommandSender.SenderId].force >= CustomConfig.admins[$"{ev.Player.GroupName}"].force)
+                            {
+                                ev.ReplyMessage = CustomConfig.LimitForceMessage;
+                                ev.Success = false;
+                                ev.Allowed = false;
+                            }
+                            else CustomConfig.admins[ev.CommandSender.SenderId].force++;
+                        }
+
+                        if (ev.Name == "effect")
+                        {
+                            if (CustomConfig.admins[ev.CommandSender.SenderId].effect >= CustomConfig.admins[$"{ev.Player.GroupName}"].effect)
+                            {
+                                ev.ReplyMessage = CustomConfig.LimitEffectMessage;
+                                ev.Success = false;
+                                ev.Allowed = false;
+                            }
+                            else CustomConfig.admins[ev.CommandSender.SenderId].effect++;
+                        }
+                        if (ev.Name == "server_event")
+                        {
+                            Log.Info($"{ev.Name} {ev.Args[0]}");
+                            ev.Allowed = false;
+                            if (ev.Args[0].ToLower() == "force_mtf_respawn")
+                            {
+                                ev.Success = false;
+                                ev.ReplyMessage = CustomConfig.ForceMTFMessage;
+                            }
+                            else if (ev.Args[0].ToLower() == "force_ci_respawn")
+                            {
+                                ev.Success = false;
+                                ev.ReplyMessage = CustomConfig.ForceCIMessage;
+                            }
+                        }
+                        if (ev.Name == "setconfig")
+                        {
+                            Log.Info($"{ev.Name} {ev.Args[0]}");
+                            ev.Allowed = false;
+                            ev.Success = false;
+                            ev.ReplyMessage = CustomConfig.FFMessage;
+                        }
+                        /*
+                        if (ev.Name == "mute" || ev.Name == "unmute")
+                        {
+                            Log.Info($"{ev.Name} {ev.Args[0]}");
+                            ev.Allowed = false;
+                            ev.Success = false;
+                            ev.ReplyMessage = "<color=red>mutes > Не мутить людей!</color>";
+                        }*/
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AntiAbus/Config.cs
@@ -0,0 +1,91 @@
+﻿using Qurre.API.Addons;
+using System.Collections.Generic;
+using System.ComponentModel;
+using static AntiAbus.AntiAbus;
+
+namespace AntiAbus
+{
+    public class Config : IConfig
+    {
+        [Description("Plugin Name")]
+        public string Name { get; set; } = "AntiAbus";
+
+        [Description("Enable the plugin?")]
+        public bool IsEnable { get; set; } = true;
+        [Description("Disable the ability to use the admin panel before the round start?")]
+        public bool IsRoundNotStarted { get; set; } = true;
+        [Description("Wait message.")]
+        public string Wait { get; set; } = "Wait";
+        [Description("Seconds message.")]
+        public string Seconds { get; set; } = "seconds";
+        [Description("RA message before the round start.")]
+        public string RoundNotStartedMessage { get; set; } = "<color=red>Round > The round has not started!</color>";
+        [Description("RA give MicroHid message.")]
+        public string GiveHidMessage { get; set; } = "<color=red>Give > There is only one MicroHid on the map!</color>";
+        [Description("RA give limit message.")]
+        public string LimitGiveMessage { get; set; } = "<color=red>Give > The limit for issuing items has been exhausted!</color>";
+        [Description("RA force limit message.")]
+        public string LimitForceMessage { get; set; } = "<color=red>Force > The limit for issuing roles has been exhausted!</color>";
+        [Description("RA effect limit message.")]
+        public string LimitEffectMessage { get; set; } = "<color=red>Effect > The limit for issuing effects has been exhausted!</color>";
+        [Description("RA force MTF message.")]
+        public string ForceMTFMessage { get; set; } = "<color=red>MTF > You can't spawn MTF</color>";
+        [Description("RA force CI message.")]
+        public string ForceCIMessage { get; set; } = "<color=red>CI > You can't spawn CI</color>";
+        [Description("RA using FF message.")]
+        public string FFMessage { get; set; } = "<color=red>FF > You can 't turn FF</color>";
+        [Description("How many seconds do need to wait for the admin panel to work?")]
+        public int NeedTimeMinutes { get; set; } = 3;
+        [Description("Dictionary: Admin, effect, force, give, heal?")]
+
+        public Dictionary<string, Admin> admins { get; set; } = new Dictionary<string, Admin>()
+        {
+            ["owner"] = new Admin()
+            {
+                give = 5,
+                force = 4,
+                effect = 2,
+                heal = 5
+            },
+            ["gladcat"] = new Admin()
+            {
+                give = 3,
+                force = 3,
+                effect = 1,
+                heal = 3
+            },
+            ["admin"] = new Admin()
+            {
+                give = 2,
+                force = 2,
+                effect = 0,
+                heal = 2
+            },
+            ["vip"] = new Admin()
+            {
+                give = 2,
+                force = 1,
+                effect = 0,
+                heal = 1
+            }
+        };
+        public class Admin
+        {
+            public Admin()
+            {
+                this.give = 0;
+                this.force = 0;
+                this.effect = 0;
+                this.heal = 0;
+            }
+
+            public int give { get; set; }
+
+            public int force { get; set; }
+
+            public int effect { get; set; }
+
+            public int heal { get; set; }
+        }
+    }
+}
