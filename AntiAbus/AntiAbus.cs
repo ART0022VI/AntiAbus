4a0ac8a1e5bb22376ed6c915e01cbdcab050f1c6
KoT0XleB <lapka.roman@yandex.ru>
2022-07-10 17:35:04 +0500

    update

--- AntiAbus/AntiAbus.cs
+++ AntiAbus/AntiAbus.cs
@@ -7,15 +7,15 @@ using Qurre.API.Objects;
 using Qurre.API.Controllers;
 using System.Collections.Generic;
 using static AntiAbus.Config;
+using System.Linq;
 
 namespace AntiAbus
 {
     public class AntiAbus : Plugin
     {
-        public static bool Enabled { get; internal set; }
-        public override string Developer => "KoToXleB#4663";
+        public override string Developer => "KoT0XleB#4663";
         public override string Name => "AntiAbus";
-        public override Version Version => new Version(1, 0, 0);
+        public override Version Version => new Version(2, 1, 0);
         public override int Priority => int.MinValue;
         public override void Enable() => RegisterEvents();
         public override void Disable() => UnregisterEvents();
@@ -27,6 +27,7 @@ namespace AntiAbus
             if (!CustomConfig.IsEnable) return;
 
             Qurre.Events.Server.SendingRA += OnSendRA;
+            Qurre.Events.Round.Restart += OnRestart;
         }
         public void UnregisterEvents()
         {
@@ -34,42 +35,79 @@ namespace AntiAbus
             if (!CustomConfig.IsEnable) return;
 
             Qurre.Events.Server.SendingRA -= OnSendRA;
+            Qurre.Events.Round.Restart -= OnRestart;
+        }
+        public void OnRestart()
+        {
+            CustomConfig.Reload();
+        }
+        public void AddDonatorAdmin(SendingRAEvent ev)
+        {
+            // Если админ не хост
+            if (!(ev.Player.IsHost) && !(ev.Player.Sender.SenderId == null) && !(ev.Player.Sender.SenderId == "1"))
+            {
+                // Если роль амдина содержится в конфиге
+                if (CustomConfig.admins.ContainsKey($"{ev.Player.GroupName}"))
+                {
+                    // Если админ не содержится в конфиге, то мы его добавляем
+                    if (!CustomConfig.admins.ContainsKey(ev.Player.Sender.SenderId))
+                    {
+                        CustomConfig.admins.Add(ev.Player.Sender.SenderId, new Admin());
+                    }
+                }
+            }
         }
         public void OnSendRA(SendingRAEvent ev)
         {
-            if (!(ev.CommandSender.SenderId == null))
+            // Добавление админа в конфиги
+            AddDonatorAdmin(ev);
+            // Содержится ли админ в конфигах
+            if (CustomConfig.admins.ContainsKey(ev.Player.Sender.SenderId))
             {
-                if (!(ev.CommandSender.SenderId == "1"))
+                // Если ожидание раунда
+                if (Round.Waiting)
                 {
-                    if (CustomConfig.admins.ContainsKey($"{ev.Player.GroupName}"))
+                    if (ev.Name == "forcestart")
                     {
-                        if (!CustomConfig.admins.ContainsKey(ev.CommandSender.SenderId))
-                        {
-                            CustomConfig.admins.Add(ev.CommandSender.SenderId, new Admin());
-                        }
+                        ev.ReplyMessage = "<color=green>Раунд > Вы начали раунд!</color>";
+                        ev.Success = true;
+                        ev.Allowed = true;
+                        return;
                     }
-                    else return;
-                    if (!Round.Started)
+                    else if (CustomConfig.IsRoundWaiting)
                     {
-                        if (CustomConfig.IsRoundNotStarted)
-                        {
-                            ev.ReplyMessage = CustomConfig.RoundNotStartedMessage;
-                            ev.Success = false;
-                            ev.Allowed = false;
-                            return;
-                        }
+                        ev.ReplyMessage = CustomConfig.RoundWaitingMessage;
+                        ev.Success = false;
+                        ev.Allowed = false;
+                        return;
                     }
-                    else
+                }
+                // Если конец раунда
+                if (Round.Ended)
+                {
+                    if (CustomConfig.IsRoundEnded)
                     {
-                        if (Round.ElapsedTime.Minutes < CustomConfig.NeedTimeMinutes)
+                        ev.ReplyMessage = CustomConfig.RoundEndedMessage;
+                        ev.Success = false;
+                        ev.Allowed = false;
+                        return;
+                    }
+                }
+                // Перед началом раунда
+                //if (Round.ElapsedTime.Minutes < CustomConfig.NeedTimeMinutes)
+                //{
+                //    ev.ReplyMessage = $"<color=red>Время > {CustomConfig.Wait} {CustomConfig.NeedTimeMinutes * 60 - Round.ElapsedTime.Seconds} {CustomConfig.Seconds}</color>";
+                //    ev.Success = false;
+                 //   ev.Allowed = false;
+                //    return;
+                //}
+                // Ввод команды с админ панели
+                switch (ev.Name)
+                {
+                    // Выдача предметов
+                    case "give":
                         {
-                            ev.ReplyMessage = $"<color=red>Time > {CustomConfig.Wait} {CustomConfig.NeedTimeMinutes * 60 - Round.ElapsedTime.Seconds} {CustomConfig.Seconds}</color>";
-                            ev.Success = false;
-                            ev.Allowed = false;
-                            return;
-                        }
-                        if (ev.Name == "give")
-                        {
+                            // Пылесос
                             if (ev.Args[1] == "16")
                             {
                                 ev.ReplyMessage = CustomConfig.GiveHidMessage;
@@ -82,62 +120,234 @@ namespace AntiAbus
                                 ev.ReplyMessage = CustomConfig.LimitGiveMessage;
                                 ev.Success = false;
                                 ev.Allowed = false;
+                                return;
                             }
                             else CustomConfig.admins[ev.CommandSender.SenderId].give++;
                         }
-
-                        if (ev.Name == "forceclass")
+                        break;
+                    // Выдача ролей
+                    case "forceclass":
                         {
                             if (CustomConfig.admins[ev.CommandSender.SenderId].force >= CustomConfig.admins[$"{ev.Player.GroupName}"].force)
                             {
                                 ev.ReplyMessage = CustomConfig.LimitForceMessage;
                                 ev.Success = false;
                                 ev.Allowed = false;
+                                return;
                             }
                             else CustomConfig.admins[ev.CommandSender.SenderId].force++;
                         }
-
-                        if (ev.Name == "effect")
+                        break;
+                    // Выдача эффектов
+                    case "effect":
                         {
                             if (CustomConfig.admins[ev.CommandSender.SenderId].effect >= CustomConfig.admins[$"{ev.Player.GroupName}"].effect)
                             {
                                 ev.ReplyMessage = CustomConfig.LimitEffectMessage;
                                 ev.Success = false;
                                 ev.Allowed = false;
+                                return;
                             }
-                            else CustomConfig.admins[ev.CommandSender.SenderId].effect++;
+                            else
+                            {
+                                CustomConfig.admins[ev.CommandSender.SenderId].effect++;
+                            }
                         }
-                        if (ev.Name == "server_event")
+                        break;
+                    // Выдача ноуклипа
+                    case "noclip":
                         {
-                            Log.Info($"{ev.Name} {ev.Args[0]}");
+                            ev.ReplyMessage = "Извините, донатеры, КотоХлеб выключил Ноуклип.";
+                            ev.Success = false;
                             ev.Allowed = false;
-                            if (ev.Args[0].ToLower() == "force_mtf_respawn")
+                        }
+                        break;
+                    // Выдача ноуклипа
+                    case "god":
+                        {
+                            ev.ReplyMessage = "Извините, донатеры, КотоХлеб выключил Годмод.";
+                            ev.Success = false;
+                            ev.Allowed = false;
+                        }
+                        break;
+                    // Спавн отрядов
+                    case "server_event":
+                        {
+                            //Log.Info($"{ev.Name} {ev.Args[0]}");
+                            if (ev.Args[0].ToLower() == "force_mtf_respawn" || ev.Args[0].ToLower() == "force_ci_respawn")
                             {
-                                ev.Success = false;
-                                ev.ReplyMessage = CustomConfig.ForceMTFMessage;
-                            }
-                            else if (ev.Args[0].ToLower() == "force_ci_respawn")
-                            {
-                                ev.Success = false;
-                                ev.ReplyMessage = CustomConfig.ForceCIMessage;
+                                if (CustomConfig.admins[ev.CommandSender.SenderId].call >= CustomConfig.admins[$"{ev.Player.GroupName}"].call)
+                                {
+                                    ev.ReplyMessage = CustomConfig.ForceMTForChaosMessage;
+                                    ev.Success = false;
+                                    ev.Allowed = false;
+                                }
+                                else
+                                {
+                                    ev.ReplyMessage = "<color=green>Отряды > Вы вызвали отряды МОГ или Хаос</color>";
+                                    CustomConfig.admins[ev.CommandSender.SenderId].call++;
+                                }
                             }
                         }
-                        if (ev.Name == "setconfig")
+                        break;
+                    // Огонь по своим
+                    case "setconfig":
                         {
-                            Log.Info($"{ev.Name} {ev.Args[0]}");
+                            //Log.Info($"{ev.Name} {ev.Args[0]}");
                             ev.Allowed = false;
                             ev.Success = false;
                             ev.ReplyMessage = CustomConfig.FFMessage;
                         }
-                        /*
-                        if (ev.Name == "mute" || ev.Name == "unmute")
+                        break;
+                    // Изменение размера
+                    case "size":
                         {
-                            Log.Info($"{ev.Name} {ev.Args[0]}");
-                            ev.Allowed = false;
-                            ev.Success = false;
-                            ev.ReplyMessage = "<color=red>mutes > Не мутить людей!</color>";
-                        }*/
-                    }
+                            if (ev.Args.Length <= 0)
+                            {
+                                ev.ReplyMessage = "Введите size [ID] [X] [Y] [Z]";
+                                ev.Success = false;
+                                ev.Allowed = false;
+                                return;
+                            }
+                            if (ev.Args.Length != 4)
+                            {
+                                ev.ReplyMessage = "Введите size [ID] [X] [Y] [Z]";
+                                ev.Success = false;
+                                ev.Allowed = false;
+                                return;
+                            }
+                            if (ev.Args[0] == ev.Player.Id.ToString())
+                            {
+                                if (float.Parse(ev.Args[1]) >= 0.85f && float.Parse(ev.Args[1]) <= 1.15f) // size 2 x
+                                {
+                                    if (float.Parse(ev.Args[2]) >= 0.85f && float.Parse(ev.Args[1]) <= 1.15f) // size 2 x y
+                                    {
+                                        if (float.Parse(ev.Args[3]) >= 0.85f && float.Parse(ev.Args[3]) <= 1.15f) // size 2 x y z
+                                        {
+                                            ev.Success = true;
+                                            ev.Allowed = true;
+                                            return;
+                                        }
+                                        else
+                                        {
+                                            ev.ReplyMessage = "Размер не должен быть больше 1.15 или меньше 0.85";
+                                            ev.Success = false;
+                                            ev.Allowed = false;
+                                            return;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        ev.ReplyMessage = "Размер не должен быть больше 1.15 или меньше 0.85";
+                                        ev.Success = false;
+                                        ev.Allowed = false;
+                                        return;
+                                    }
+                                }
+                                else
+                                {
+                                    ev.ReplyMessage = "Размер не должен быть больше 1.15 или меньше 0.85";
+                                    ev.Success = false;
+                                    ev.Allowed = false;
+                                    return;
+                                }
+                            }
+                        }
+                        break;
+                    // Изменение масштаба
+                    case "scale":
+                        {
+                            if (ev.Args.Length <= 0)
+                            {
+                                ev.ReplyMessage = "Введите scale [Размер]";
+                                ev.Success = false;
+                                ev.Allowed = false;
+                                return;
+                            }
+                            if (ev.Args.Length != 2)
+                            {
+                                ev.ReplyMessage = "Введите scale [Размер]";
+                                ev.Success = false;
+                                ev.Allowed = false;
+                                return;
+                            }
+                            if (ev.Args[0] != ev.Player.Id.ToString())
+                            {
+                                ev.ReplyMessage = "Вы можете только себе изменить размер.";
+                                ev.Success = false;
+                                ev.Allowed = false;
+                                return;
+                            }
+                            if (float.Parse(ev.Args[1]) >= 0.85f && float.Parse(ev.Args[1]) <= 1.15f)
+                            {
+                                ev.Success = true;
+                                ev.Allowed = true;
+                                return;
+                            }
+                            else
+                            {
+                                ev.ReplyMessage = "Размер не должен быть больше 1.15 или меньше 0.85";
+                                ev.Success = false;
+                                ev.Allowed = false;
+                                return;
+                            }
+                        }
+                    // Выдача хилки
+                    case "hp":
+                        {
+                            if (ev.Args.Length <= 0)
+                            {
+                                ev.ReplyMessage = "<color=red>Введите hp [id] [amount]</color>";
+                                ev.Success = false;
+                                ev.Allowed = false;
+                                return;
+                            }
+                            if (ev.Args.Length != 2)
+                            {
+                                ev.ReplyMessage = "<color=red>Введите hp [id] [amount]</color>";
+                                ev.Success = false;
+                                ev.Allowed = false;
+                                return;
+                            }
+                            if (int.Parse(ev.Args[0]) != ev.Player.Id)
+                            {
+                                ev.ReplyMessage = "<color=red>Введите свой айди: hp [id] [amount]</color>";
+                                ev.Success = false;
+                                ev.Allowed = false;
+                                return;
+                            }
+                            if (CustomConfig.admins[ev.CommandSender.SenderId].heal >= CustomConfig.admins[$"{ev.Player.GroupName}"].heal)
+                            {
+                                ev.ReplyMessage = "<color=red>Хилка > Вы исчерпали свой лимит выдачи ХП</color>";
+                                ev.Success = false;
+                                ev.Allowed = false;
+                            }
+                            else
+                            {
+                                var hp = 50;
+                                if (ev.Player.Team == Team.SCP)
+                                {
+                                    if (Int32.Parse(ev.Args[1]) >= 1000)
+                                    {
+                                        hp = 500;
+                                    }
+                                }
+                                else if (ev.Player.Team != Team.SCP)
+                                {
+                                    if (Int32.Parse(ev.Args[1]) >= 100)
+                                    {
+                                        hp = 100;
+                                    }
+                                }
+                                ev.Player.Hp = hp;
+                                ev.Allowed = false;
+                                ev.Success = true;
+                                ev.ReplyMessage = $"<color=green>Хилка > Вам было выдано {hp} ХП.</color>";
+                                CustomConfig.admins[ev.CommandSender.SenderId].heal++;
+                            }
+                        }
+                        break;
+                    default: return;
                 }
             }
         }
--- AntiAbus/Config.cs
+++ AntiAbus/Config.cs
@@ -13,27 +13,31 @@ namespace AntiAbus
         [Description("Enable the plugin?")]
         public bool IsEnable { get; set; } = true;
         [Description("Disable the ability to use the admin panel before the round start?")]
-        public bool IsRoundNotStarted { get; set; } = true;
+        public bool IsRoundWaiting { get; set; } = true;
+        [Description("Disable the ability to use the admin panel after the end of the round?")]
+        public bool IsRoundEnded { get; set; } = true;
         [Description("Wait message.")]
-        public string Wait { get; set; } = "Wait";
+        public string Wait { get; set; } = "Подождите"; // "Wait";
         [Description("Seconds message.")]
-        public string Seconds { get; set; } = "seconds";
+        public string Seconds { get; set; } = "секунд"; // "seconds";
         [Description("RA message before the round start.")]
-        public string RoundNotStartedMessage { get; set; } = "<color=red>Round > The round has not started!</color>";
+        public string RoundWaitingMessage { get; set; } = "<color=red>Раунд > Раунд ещё не начался!</color>"; // "<color=red>Round > The round has not started!</color>";
+        [Description("RA message before the round start.")]
+        public string RoundEndedMessage { get; set; } = "<color=red>Раунд > Раунд ещё не закончился!</color>"; // "<color=red>Round > The round has ended!</color>";
         [Description("RA give MicroHid message.")]
-        public string GiveHidMessage { get; set; } = "<color=red>Give > There is only one MicroHid on the map!</color>";
+        public string GiveHidMessage { get; set; } = "<color=red>Предметы > На карте уже есть МикроХид!</color>"; // "<color=red>Give > There is only one MicroHid on the map!</color>";
         [Description("RA give limit message.")]
-        public string LimitGiveMessage { get; set; } = "<color=red>Give > The limit for issuing items has been exhausted!</color>";
+        public string LimitGiveMessage { get; set; } = "<color=red>Предметы > Вы исчерпали свой лимит выдачи предметов!</color>"; // "<color=red>Give > The limit for issuing items has been exhausted!</color>";
         [Description("RA force limit message.")]
-        public string LimitForceMessage { get; set; } = "<color=red>Force > The limit for issuing roles has been exhausted!</color>";
+        public string LimitForceMessage { get; set; } = "<color=red>Роли > Вы исчерпали свой лимит выдачи ролей!</color>"; // "<color=red>Force > The limit for issuing roles has been exhausted!</color>";
         [Description("RA effect limit message.")]
-        public string LimitEffectMessage { get; set; } = "<color=red>Effect > The limit for issuing effects has been exhausted!</color>";
+        public string LimitEffectMessage { get; set; } = "<color=red>Эффекты > Вы исчерпали свой лимит выдачи эффектов</color>"; // "<color=red>Effect > The limit for issuing effects has been exhausted!</color>";
         [Description("RA force MTF message.")]
-        public string ForceMTFMessage { get; set; } = "<color=red>MTF > You can't spawn MTF</color>";
-        [Description("RA force CI message.")]
-        public string ForceCIMessage { get; set; } = "<color=red>CI > You can't spawn CI</color>";
+        public string ForceMTForChaosMessage { get; set; } = "<color=red>Отряды > Вы исчерпали свой лимит на вызов отрядов Мог/Хаос</color>"; // "<color=red>MTF > You can't spawn MTF</color>";
+        //[Description("RA force CI message.")]
+        //public string ForceCIMessage { get; set; } = "<color=red>ХАОС > Ты не можешь вызвать Хаоситов</color>"; // "<color=red>CI > You can't spawn CI</color>";
         [Description("RA using FF message.")]
-        public string FFMessage { get; set; } = "<color=red>FF > You can 't turn FF</color>";
+        public string FFMessage { get; set; } = "<color=red>ФФ > Вы не можете включить 'Огонь по Своим'</color>"; // "<color=red>FF > You can 't turn FF</color>";
         [Description("How many seconds do need to wait for the admin panel to work?")]
         public int NeedTimeMinutes { get; set; } = 3;
         [Description("Dictionary: Admin, effect, force, give, heal?")]
@@ -44,27 +48,37 @@ namespace AntiAbus
             {
                 give = 5,
                 force = 4,
+                effect = 5,
+                heal = 1,
+                call = 2
+            },
+            ["gladcat"] = new Admin()
+            {
+                give = 4,
+                force = 3,
                 effect = 2,
-                heal = 5
+                heal = 1,
+                call = 1
             },
             ["admin"] = new Admin()
             {
+                give = 3,
+                force = 3,
+                effect = 1,
+                heal = 1,
+                call = 0
+            },
+            ["vip"] = new Admin()
+            {
                 give = 2,
-                force = 2,
+                force = 1,
                 effect = 0,
-                heal = 2
+                heal = 0,
+                call = 0
             }
         };
         public class Admin
         {
-            public Admin()
-            {
-                this.give = 0;
-                this.force = 0;
-                this.effect = 0;
-                this.heal = 0;
-            }
-
             public int give { get; set; }
 
             public int force { get; set; }
@@ -72,6 +86,17 @@ namespace AntiAbus
             public int effect { get; set; }
 
             public int heal { get; set; }
+
+            public int call { get; set; }
+
+            public Admin()
+            {
+                this.give = 0;
+                this.force = 0;
+                this.effect = 0;
+                this.heal = 0;
+                this.call = 0;
+            }
         }
     }
 }
